f92cca8c66a21106b1230af18898d857064c7fc6
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-26 15:45:56 -0700

    Tasks Controller

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,7 +1,10 @@
 import { remultExpress } from "remult/remult-express";
 import { Task } from "../shared/Task.js"
+import { TasksController } from "../shared/TasksController.js";
+import { TasksController } from "../shared/TasksController.js";
 
 export const api = remultExpress({
     entities: [Task],
     admin: true,
+    controllers: [TasksController]
 })
